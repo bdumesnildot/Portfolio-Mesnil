876e4eddd2f7733fc18a0af157ce9e0ed1121a55
BENOIT DU_MESNILDOT <benoit.dumesnildot@gmail.com>
2023-04-25 10:40:12 +0200

    added welcome component

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -5,6 +5,7 @@ import "../index.scss";
 import "../styles/Root.scss";
 
 import Navbar from "../components/Navbar";
+import Welcome from "../components/Welcome";
 
 function Root() {
   const location = useLocation();
@@ -18,23 +19,7 @@ function Root() {
     <div className="root-page">
       <Navbar />
       <div className="root-content">
-        {isRootPage &&
-          <svg
-            className="circular-text"
-            viewBox="0 0 300 300" 
-            xmlns="http://www.w3.org/2000/svg" 
-            xmlnsXlink="http://www.w3.org/1999/xlink">
-            <path 
-              id="SunCatcherStudio" 
-              fill="none" 
-              stroke="#c54309" 
-              strokeWidth="1"
-              d="M 32.550491,148.48008 A -108.15144,-108.15144 0 0 1 140.70194,40.328644 -108.15144,-108.15144 0 0 1 248.85338,148.48008 -108.15144,-108.15144 0 0 1 140.70194,256.63153 -108.15144,-108.15144 0 0 1 32.550491,148.48008 Z" />  
-            <text fontSize="26" fill="#231815" letterSpacing="1" fontFamily="Poppins, sans-serif">
-              <textPath xlinkHref="#SunCatcherStudio" side="left" startOffset="50">Benoit du Mesnildot | Web developer</textPath>
-            </text>
-          </svg>
-        }
+        {isRootPage && <Welcome /> }
         <Outlet />
       </div>
     </div>
